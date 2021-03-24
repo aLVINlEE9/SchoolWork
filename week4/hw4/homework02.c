0d774a21360d2db5bb4f72e3bb67c4d1d19536d0
aLVINlEE9 <fldh3746@gmail.com>
2021-03-24 21:05:38 +0900

    add 02

---
+++ week4/hw4/homework02.c
@@ -0,0 +1,59 @@
+int atoi(char *str)
+{
+	int sign = 1;
+	int ret = 0;
+	while((*str >= 9 && *str <= 13) || *str == ' ')
+		str++;
+	if(*str == '-' || *str == '+')
+	{
+		if(*str == '-')
+			sign *= -1;
+		str++;
+	}
+	while(*str >= '0' && *str <= '9')
+	{
+		ret *= 10;
+		ret += (sign *(*str++ - '0'));
+	}
+	return ret;
+}
+
+void sort_num(int *i, int *j, int *k)
+{
+	int arr[3] = {*i, *j, *k};
+	for(int n = 0; n < 3; n++)
+	{
+		for(int m = 0; m < 3 - n - 1; m++)
+		{
+			if(arr[m] > arr[m + 1])
+			{
+				int temp;
+				temp = arr[m];
+				arr[m] = arr[m + 1];
+				arr[m + 1] = temp;
+			}
+		}
+	}
+	*i = arr[0];
+	*j = arr[1];
+	*k = arr[2];
+	return ;
+}
+
+#include<unistd.h>
+
+int main(int argc, char **argv)
+{
+	if(argc == 4)
+	{
+		int i, j, k;
+		i = atoi(argv[1]);
+		j = atoi(argv[2]);
+		k = atoi(argv[3]);
+		sort_num(&i, &j, &k);
+		write(1, "all integer inputs were swapped in order\n", 41);
+		return 0;
+	}
+	write(1, "error\n", 6);
+	return 0;
+}
