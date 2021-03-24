34cf9aedaf8c831bba99803e1776e66579376c9d
aLVINlEE9 <fldh3746@gmail.com>
2021-03-24 20:06:12 +0900

    add h1

---
+++ week4/hw4/homework01.c
@@ -0,0 +1,43 @@
+#include<unistd.h>
+#include<stdlib.h>
+
+char *strrev(char *str, int cnt)
+{
+	int i = -1;
+	char *ret;
+	char *rec;
+	if(!(ret = malloc(sizeof(char) * (cnt + 1))))
+		return NULL;
+	while(*str++)
+		;
+	str -= 2;
+	rec = ret;
+	while(++i < cnt)
+		*ret++ = *str--;
+	ret[i] = 0;
+	return rec;
+}
+
+void print_str(char *str)
+{
+	while(*str)
+		write(1, str++, 1);
+	return ;
+}
+
+int main(int argc, char **argv)
+{
+	char *ret;
+	if(argc == 2)
+	{
+		int cnt = 0;
+		while(argv[1][cnt++])
+			;
+		ret = strrev(argv[1], cnt - 1);
+		print_str(ret);
+		free(ret);
+		return 0;
+	}
+	write(2, "error\n", 5);
+	return 0;
+}
